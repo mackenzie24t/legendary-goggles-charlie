e27217b8840ba27add5ba77eb5d18b5da4fdffe4
mackenzie24t <157424806+mackenzie24t@users.noreply.github.com>
2024-02-07 14:26:26 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-  
-  //TODO: if position is on the far right, tell user they won
 
+  // Shows position of doge in the console
   console.log("Top: " + $("img").position().top + " Left" + $("img").position().left);
 
+  // Makes doge draggable
   $("img").draggable({
 
     stop: function() {
-      console.log("Stopping?????");
+    console.log("Stopping?????");
       let dogeLeft = $("img").position().left;
       if(dogeLeft > 500){
 
+        // Winning!!
+        // Displays winning message to user and changes doge
         console.log("we above 500");
         $('h4').text("You won!! WOW!!! Good job!");
         $('img').attr("src", "https://static.independent.co.uk/2021/05/24/20/doge.jpg");
